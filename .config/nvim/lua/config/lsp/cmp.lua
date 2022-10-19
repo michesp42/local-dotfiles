30d5c144b6dbe174dd57544d76b317fb002f4aa4
Michael B. Espeña <michael.esp42@pm.me>
2022-10-19 19:29:08 +0800

    remove comments that was from the default config

--- .config/nvim/lua/config/lsp/cmp.lua
+++ .config/nvim/lua/config/lsp/cmp.lua
@@ -18,12 +18,11 @@ end
 cmp.setup {
   snippet = {
     expand = function(args)
-      require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+      require('luasnip').lsp_expand(args.body)
     end,
   },
 
   window = {
-    -- completion = cmp.config.window.bordered(),
     documentation = cmp.config.window.bordered(),
   },
 
