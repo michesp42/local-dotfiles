cae079e45e232e2d163818af256dbc62ac411f51
Michael B. Espeña <michael.esp42@pm.me>
2022-10-20 11:27:13 +0800

    fix misconfiguration for `capabilities`
    no need to use `vim.lsp.protocol.make_client_capabilities()` in
    `default_capabilities`

--- .config/nvim/lua/config/lsp/lspconfig.lua
+++ .config/nvim/lua/config/lsp/lspconfig.lua
@@ -47,8 +47,7 @@ local servers = {
   'vimls',
 }
 
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
