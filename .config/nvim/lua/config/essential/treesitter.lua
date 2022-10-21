6ac33ee68b12dd20033e4eead2aef70229b109da
Michael B. Espeña <michael.esp42@pm.me>
2022-10-21 21:51:55 +0800

    add `incremental selection` for treesitter

--- .config/nvim/lua/config/essential/treesitter.lua
+++ .config/nvim/lua/config/essential/treesitter.lua
@@ -46,6 +46,20 @@ nvim_treesitter_config.setup {
     additional_vim_regex_highlighting = false,
   },
 
+  incremental_selection = {
+    enable = true,
+    keymaps = {
+      init_selection = 'gnn',
+      node_incremental = 'grn',
+      scope_incremental = 'grc',
+      node_decremental = 'grm',
+    },
+  },
+
+  indent = {
+    enable = true,
+  },
+
   rainbow = {
     enable = true,
     extended_mode = true,
