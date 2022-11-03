c41d46cc7c0847fe0e795ba9905d2605a7fd0aa2
Michael B. Espeña <michael.esp42@pm.me>
2022-11-03 09:32:31 +0800

    [neovim] rm nordic.nvim

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -79,7 +79,6 @@ packer.startup(function(use)
   use { 'RRethy/nvim-treesitter-endwise', requires = 'nvim-treesitter/nvim-treesitter' }
 
   -- colorscheme
-  -- use 'andersevenrud/nordic.nvim'
   use 'navarasu/onedark.nvim'
 
   -- visualizing undo history
