70ba93745a7ae653c8f7edc17b8453dccc823ec3
Michael B. Espeña <michael.esp42@pm.me>
2022-11-05 09:11:06 +0800

    [neovim] change LSP diagnostics icons
    change icons to more easily visible ones

--- .config/nvim/lua/config/lsp/lspconfig.lua
+++ .config/nvim/lua/config/lsp/lspconfig.lua
@@ -89,7 +89,7 @@ lspconfig.sumneko_lua.setup {
 --   }
 -- }
 
-local signs = { Error = ' ', Warn = ' ', Hint = ' ', Info = ' ' }
+local signs = { Error = ' ', Warn = ' ', Hint = ' ', Info = ' ' }
 
 for type, icon in pairs(signs) do
   local hl = 'DiagnosticSign' .. type
