ed71a5a8e86e35d407ff64cd13eacf66b2e9ba29
Michael B. Espeña <michael.esp42@pm.me>
2022-11-09 17:00:57 +0800

    [neovim] update `FTerm` config
    - increase size of FTerm window
    - adjust placement of FTerm window to be lower so that it covers the
      status bar
    - reformat using stylua

--- .config/nvim/lua/config/helpful/terminal.lua
+++ .config/nvim/lua/config/helpful/terminal.lua
@@ -4,12 +4,12 @@ if not ok then
   return
 end
 
-fterm.setup({
-  border = "single",
+fterm.setup {
+  border = 'single',
   dimensions = {
-    height = 0.3,
+    height = 0.4,
     width = 1.0,
     x = 0,
-    y = 0.9,
+    y = 1.0,
   },
-})
+}
