f2ea11bd1813cc1c46837b30e993639d1f1c6625
Michael B. Espeña <michael.esp42@pm.me>
2022-11-26 21:15:22 +0800

    [nvim] fallback to default lspkind cmp format
    nvim-cmp completions error says something like "attempt to concatenate a
    nil value" but I can't deal with this bug(?) right now so I'm using the
    default lspkind.cmp_format which seems to work.
    
    The completion errors happens, as I've observed in Rust files,
    especially when I'm typing out something that has generics or lifetime
    annotations.

--- .config/nvim/lua/config/lsp/cmp.lua
+++ .config/nvim/lua/config/lsp/cmp.lua
@@ -77,15 +77,16 @@ cmp.setup {
   },
 
   formatting = {
-    fields = { 'kind', 'abbr', 'menu' },
-    format = function(entry, vim_item)
-      local kind = lspkind.cmp_format { mode = 'symbol_text', maxwidth = 50 }(entry, vim_item)
-      local strings = vim.split(kind.kind, '%s', { trimempty = true })
-      kind.kind = ' ' .. strings[1] .. ' '
-      kind.menu = '    (' .. strings[2] .. ')'
-
-      return kind
-    end,
+    format = lspkind.cmp_format(),
+    --   fields = { 'kind', 'abbr', 'menu' },
+    --   format = function(entry, vim_item)
+    --     local kind = lspkind.cmp_format { mode = 'symbol_text', maxwidth = 50 }(entry, vim_item)
+    --     local strings = vim.split(kind.kind, '%s', { trimempty = true })
+    --     kind.kind = ' ' .. strings[1] .. ' '
+    --     kind.menu = '    (' .. strings[2] .. ')'
+    --
+    --     return kind
+    --   end,
   },
 }
 
