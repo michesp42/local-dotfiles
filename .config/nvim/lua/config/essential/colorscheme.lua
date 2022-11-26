4b101f90eff18abb2a7827687d3a390bc00cfaed
Michael B. Espeña <michael.esp42@pm.me>
2022-11-26 21:19:16 +0800

    [nvim] comment out custom highlights for now
    Since I'm using the default lspkind.cmp_format, I'm turning off my
    custom highlights for the theme which does not look that good with the
    default format.

--- .config/nvim/lua/config/essential/colorscheme.lua
+++ .config/nvim/lua/config/essential/colorscheme.lua
@@ -18,40 +18,40 @@ vim.api.nvim_set_hl(0, '@method', { fg = '#88c0d0' })
 vim.api.nvim_set_hl(0, 'PmenuSel', { bg = '#373e4d', fg = 'NONE' })
 vim.api.nvim_set_hl(0, 'Pmenu', { fg = '#C5CDD9', bg = '#2b303b' })
 
-vim.api.nvim_set_hl(0, 'CmpItemAbbrDeprecated', { fg = '#7E8294', bg = 'NONE' })
-vim.api.nvim_set_hl(0, 'CmpItemAbbrMatch', { fg = '#88c0d0', bg = 'NONE' })
-vim.api.nvim_set_hl(0, 'CmpItemAbbrMatchFuzzy', { fg = '#88c0d0', bg = 'NONE' })
-vim.api.nvim_set_hl(0, 'CmpItemMenu', { fg = '#b48ead', bg = 'NONE' })
-
-vim.api.nvim_set_hl(0, 'CmpItemKindField', { fg = '#EED8DA', bg = '#bf616a' })
-vim.api.nvim_set_hl(0, 'CmpItemKindProperty', { fg = '#EED8DA', bg = '#bf616a' })
-vim.api.nvim_set_hl(0, 'CmpItemKindEvent', { fg = '#EED8DA', bg = '#bf616a' })
-
-vim.api.nvim_set_hl(0, 'CmpItemKindText', { fg = '#C3E88D', bg = '#a3be8c' })
-vim.api.nvim_set_hl(0, 'CmpItemKindEnum', { fg = '#C3E88D', bg = '#a3be8c' })
-vim.api.nvim_set_hl(0, 'CmpItemKindKeyword', { fg = '#C3E88D', bg = '#a3be8c' })
-
-vim.api.nvim_set_hl(0, 'CmpItemKindConstant', { fg = '#FFE082', bg = '#D4BB6C' })
-vim.api.nvim_set_hl(0, 'CmpItemKindConstructor', { fg = '#FFE082', bg = '#D4BB6C' })
-vim.api.nvim_set_hl(0, 'CmpItemKindReference', { fg = '#FFE082', bg = '#D4BB6C' })
-
-vim.api.nvim_set_hl(0, 'CmpItemKindFunction', { fg = '#EADFF0', bg = '#b48ead' })
-vim.api.nvim_set_hl(0, 'CmpItemKindStruct', { fg = '#EADFF0', bg = '#b48ead' })
-vim.api.nvim_set_hl(0, 'CmpItemKindClass', { fg = '#EADFF0', bg = '#b48ead' })
-vim.api.nvim_set_hl(0, 'CmpItemKindModule', { fg = '#EADFF0', bg = '#b48ead' })
-vim.api.nvim_set_hl(0, 'CmpItemKindOperator', { fg = '#EADFF0', bg = '#b48ead' })
-
-vim.api.nvim_set_hl(0, 'CmpItemKindVariable', { fg = '#C5CDD9', bg = '#7E8294' })
-vim.api.nvim_set_hl(0, 'CmpItemKindFile', { fg = '#C5CDD9', bg = '#7E8294' })
-
-vim.api.nvim_set_hl(0, 'CmpItemKindUnit', { fg = '#F5EBD9', bg = '#D4A959' })
-vim.api.nvim_set_hl(0, 'CmpItemKindSnippet', { fg = '#F5EBD9', bg = '#D4A959' })
-vim.api.nvim_set_hl(0, 'CmpItemKindFolder', { fg = '#F5EBD9', bg = '#D4A959' })
-
-vim.api.nvim_set_hl(0, 'CmpItemKindMethod', { fg = '#DDE5F5', bg = '#81a1c1' })
-vim.api.nvim_set_hl(0, 'CmpItemKindValue', { fg = '#DDE5F5', bg = '#81a1c1' })
-vim.api.nvim_set_hl(0, 'CmpItemKindEnumMember', { fg = '#DDE5F5', bg = '#81a1c1' })
-
-vim.api.nvim_set_hl(0, 'CmpItemKindInterface', { fg = '#D8EEEB', bg = '#8fbcbb' })
-vim.api.nvim_set_hl(0, 'CmpItemKindColor', { fg = '#D8EEEB', bg = '#8fbcbb' })
-vim.api.nvim_set_hl(0, 'CmpItemKindTypeParameter', { fg = '#D8EEEB', bg = '#8fbcbb' })
+-- vim.api.nvim_set_hl(0, 'CmpItemAbbrDeprecated', { fg = '#7E8294', bg = 'NONE' })
+-- vim.api.nvim_set_hl(0, 'CmpItemAbbrMatch', { fg = '#88c0d0', bg = 'NONE' })
+-- vim.api.nvim_set_hl(0, 'CmpItemAbbrMatchFuzzy', { fg = '#88c0d0', bg = 'NONE' })
+-- vim.api.nvim_set_hl(0, 'CmpItemMenu', { fg = '#b48ead', bg = 'NONE' })
+--
+-- vim.api.nvim_set_hl(0, 'CmpItemKindField', { fg = '#EED8DA', bg = '#bf616a' })
+-- vim.api.nvim_set_hl(0, 'CmpItemKindProperty', { fg = '#EED8DA', bg = '#bf616a' })
+-- vim.api.nvim_set_hl(0, 'CmpItemKindEvent', { fg = '#EED8DA', bg = '#bf616a' })
+--
+-- vim.api.nvim_set_hl(0, 'CmpItemKindText', { fg = '#C3E88D', bg = '#a3be8c' })
+-- vim.api.nvim_set_hl(0, 'CmpItemKindEnum', { fg = '#C3E88D', bg = '#a3be8c' })
+-- vim.api.nvim_set_hl(0, 'CmpItemKindKeyword', { fg = '#C3E88D', bg = '#a3be8c' })
+--
+-- vim.api.nvim_set_hl(0, 'CmpItemKindConstant', { fg = '#FFE082', bg = '#D4BB6C' })
+-- vim.api.nvim_set_hl(0, 'CmpItemKindConstructor', { fg = '#FFE082', bg = '#D4BB6C' })
+-- vim.api.nvim_set_hl(0, 'CmpItemKindReference', { fg = '#FFE082', bg = '#D4BB6C' })
+--
+-- vim.api.nvim_set_hl(0, 'CmpItemKindFunction', { fg = '#EADFF0', bg = '#b48ead' })
+-- vim.api.nvim_set_hl(0, 'CmpItemKindStruct', { fg = '#EADFF0', bg = '#b48ead' })
+-- vim.api.nvim_set_hl(0, 'CmpItemKindClass', { fg = '#EADFF0', bg = '#b48ead' })
+-- vim.api.nvim_set_hl(0, 'CmpItemKindModule', { fg = '#EADFF0', bg = '#b48ead' })
+-- vim.api.nvim_set_hl(0, 'CmpItemKindOperator', { fg = '#EADFF0', bg = '#b48ead' })
+--
+-- vim.api.nvim_set_hl(0, 'CmpItemKindVariable', { fg = '#C5CDD9', bg = '#7E8294' })
+-- vim.api.nvim_set_hl(0, 'CmpItemKindFile', { fg = '#C5CDD9', bg = '#7E8294' })
+--
+-- vim.api.nvim_set_hl(0, 'CmpItemKindUnit', { fg = '#F5EBD9', bg = '#D4A959' })
+-- vim.api.nvim_set_hl(0, 'CmpItemKindSnippet', { fg = '#F5EBD9', bg = '#D4A959' })
+-- vim.api.nvim_set_hl(0, 'CmpItemKindFolder', { fg = '#F5EBD9', bg = '#D4A959' })
+--
+-- vim.api.nvim_set_hl(0, 'CmpItemKindMethod', { fg = '#DDE5F5', bg = '#81a1c1' })
+-- vim.api.nvim_set_hl(0, 'CmpItemKindValue', { fg = '#DDE5F5', bg = '#81a1c1' })
+-- vim.api.nvim_set_hl(0, 'CmpItemKindEnumMember', { fg = '#DDE5F5', bg = '#81a1c1' })
+--
+-- vim.api.nvim_set_hl(0, 'CmpItemKindInterface', { fg = '#D8EEEB', bg = '#8fbcbb' })
+-- vim.api.nvim_set_hl(0, 'CmpItemKindColor', { fg = '#D8EEEB', bg = '#8fbcbb' })
+-- vim.api.nvim_set_hl(0, 'CmpItemKindTypeParameter', { fg = '#D8EEEB', bg = '#8fbcbb' })
