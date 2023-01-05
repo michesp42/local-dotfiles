ab80a240189765bf04cdc2ee2e85d8838421ca65
Michael B. Espeña <michael.esp42@pm.me>
2023-01-05 11:22:40 +0800

    [nvim] set custom highlights for `ts-rainbow2`

--- .config/nvim/lua/config/essential/colorscheme.lua
+++ .config/nvim/lua/config/essential/colorscheme.lua
@@ -12,3 +12,11 @@ nord.setup({
 })
 
 vim.cmd("colorscheme nord")
+
+vim.api.nvim_set_hl(0, "TSRainbowRed", { fg = "#bf616a" })
+vim.api.nvim_set_hl(0, "TSRainbowYellow", { fg = "#ebcb8b" })
+vim.api.nvim_set_hl(0, "TSRainbowBlue", { fg = "#81a1c1" })
+vim.api.nvim_set_hl(0, "TSRainbowGreen", { fg = "#a3b38c" })
+vim.api.nvim_set_hl(0, "TSRainbowCyan", { fg = "#8fbcbb" })
+vim.api.nvim_set_hl(0, "TSRainbowOrange", { fg = "#d08770" })
+vim.api.nvim_set_hl(0, "TSRainbowViolet", { fg = "#b48ead" })
